3a8336601813301e05b9a86493d92db31d5ec9cb
debalid <debalid@gmail.com>
2016-02-27 04:32:28 +0300

    Moved user info from outer container

--- public/scripts/components/userinfo-instagram.js
+++ public/scripts/components/userinfo-instagram.js
@@ -8,22 +8,18 @@ var UserInfo = React.createClass({
     render: function()
     {
         return (
-            <div className="container">
+            <div className="span4 well">
                 <div className="row">
-                    <div className="span4 well">
-                        <div className="row">
-                            <div className="span1">
-                                <a href={this.props.profileUrl} className="thumbnail">
-                                    <img src={this.props.profileImage} alt="" />
-                                </a>
-                            </div>
-                            <div className="span3">
-                                <p>{this.props.profileLogin}</p>
-                                <p><strong>{this.props.profileName}</strong></p>
-                                <span className="label label-success">{this.props.profilePhotosCount} фото</span>
-                                <span className="label label-primary">{this.props.profileFollowersCount} подписчиков</span>
-                            </div>
-                        </div>
+                    <div className="span1">
+                        <a href={this.props.profileUrl} target="_blank" className="thumbnail">
+                            <img src={this.props.profileImage} alt="" />
+                        </a>
+                    </div>
+                    <div className="span3">
+                        <p>{this.props.profileLogin}</p>
+                        <p><strong>{this.props.profileName}</strong></p>
+                        <span className="label label-success">{this.props.profilePhotosCount} фото</span>
+                        <span className="label label-primary">{this.props.profileFollowersCount} подписчиков</span>
                     </div>
                 </div>
             </div>
