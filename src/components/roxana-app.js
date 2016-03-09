f8c642ecd66e04dbebd3e5d5ca3c95a0ffe6217b
Blakhard <vlad.abdulmianov@gmail.com>
2016-03-09 12:06:03 +0300

    Add some responsive for mobiles

--- src/components/roxana-app.js
+++ src/components/roxana-app.js
@@ -10,28 +10,37 @@ var MapContainer = require('./map/map-container');
 
 var RoxanaApp = React.createClass({
     getInitialState: function(){
-        return {swiped: false}
+        return {menuOpen: false}
     },
     swipedRight: function(){
         console.log("I work")
-        this.setState({swiped: true})
+        this.setState({menuOpen: true})
     },
     swipedLeft: function(){
-        this.setState({swiped: false})
+        this.setState({menuOpen: false})
+    },
+    linkClick: function(){
+        if (!this.state.menuOpen){this.setState({menuOpen: true})}
+        else {this.setState({menuOpen: false})}
+
     },
     render: function()
     {
         var classname = classNames({
-            'active': this.state.swiped,
-            '': !this.state.swiped
+            'active': this.state.menuOpen,
+            '': !this.state.menuOpen
+        });
+        var linkClass = classNames({
+            'menu-link active': this.state.menuOpen,
+            'menu-link': !this.state.menuOpen
         });
         return (
             <Swipable onSwipedRight={this.swipedRight} onSwipedLeft={this.swipedLeft}>
                 <div className="heighted">
                     <div className="header">
-                        <img src="style/image/logo.svg" alt=""/><span><b>ay To Go!</b></span>
+                        <img src="style/image/logo.svg" alt=""/><span id="headerSpan"><b>ay To Go!</b></span>
                     </div>
-                    <a href="#menu" id="menuLink" className="menu-link">
+                    <a href="#menu" id="menuLink" className={linkClass} onClick={this.linkClick}>
                         <span></span>
                     </a>
                     <Menu propClass={classname}/>
