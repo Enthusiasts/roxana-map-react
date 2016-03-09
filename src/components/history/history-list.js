85d9a8b07bd4b0a5b0624503f77ca89ccd9498b1
Nikita Fomichev <debalid@gmail.com>
2016-03-09 19:18:42 +0300

    :Started history

---
+++ src/components/history/history-list.js
@@ -0,0 +1,32 @@
+/**
+ * Created by debalid on 09.03.2016.
+ */
+var React = require('react');
+
+var HistoryItem = require('./history-item');
+
+var HistoryList = React.createClass({
+    renderItems: function()
+    {
+        var items = this.props.items;
+        return items
+            ? items.map(route => <HistoryItem route={route}/>)
+            : null;
+    },
+
+    render: function()
+    {
+        return (
+            <div>
+                {this.renderItems()}
+            </div>
+        );
+    }
+});
+
+HistoryList.propTypes = {
+    isAuthorized: React.PropTypes.boolean.isRequired,
+    items: React.PropTypes.array.isRequired
+};
+
+module.exports = HistoryList;
