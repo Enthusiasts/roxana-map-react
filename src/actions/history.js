80e3072d9f8f54a538050c3a059288f6d8533cca
debalid <debalid@gmail.com>
2016-03-31 14:35:20 +0300

    Reformated history

--- src/actions/history.js
+++ src/actions/history.js
@@ -6,16 +6,14 @@ var Properites = require('../const/properties');
 var RouteActions = require('./routes');
 
 const FETCH_USER_HISTORY_BEGIN = 'FETCH_USER_HISTORY_BEGIN';
-const fetchUserHistoryBegin = function()
-{
+const fetchUserHistoryBegin = function () {
     return {
         type: FETCH_USER_HISTORY_BEGIN
     };
 };
 
 const FETCH_USER_HISTORY_VALIDATE = 'FETCH_USER_HISTORY_VALIDATE';
-const fetchUserHistoryValidate = function(items)
-{
+const fetchUserHistoryValidate = function (items) {
     return {
         type: FETCH_USER_HISTORY_VALIDATE,
         payload: {
@@ -24,18 +22,15 @@ const fetchUserHistoryValidate = function(items)
     };
 };
 
-const fetchUserHistory = function(userId)
-{
-    return (dispatch) =>
-    {
+const fetchUserHistory = function (userId) {
+    return (dispatch) => {
         dispatch(fetchUserHistoryBegin());
 
         //TODO: handle with userId
-        return fetch(Properites.API.ROOT + 'routes/search/findByOwnerId?clientId='+userId)
+        return fetch(Properites.API.ROOT + 'routes/search/findByOwnerId?clientId=' + userId)
             .then(response => response.json())
             .then(
-                json =>
-                {
+                json => {
                     var routes = json._embedded.routes;
                     console.log("History: " + routes.length + " received.");
                     dispatch(fetchUserHistoryValidate(routes));
@@ -46,15 +41,12 @@ const fetchUserHistory = function(userId)
 
 const editRoute = (routeId) => _loadRoute(routeId, Properites.ROUTE.CONTEXTS.EDIT);
 const watchRoute = (routeId) => _loadRoute(routeId, Properites.ROUTE.CONTEXTS.WATCH);
-const _loadRoute = function(routeId, context)
-{
-    return (dispatch) =>
-    {
+const _loadRoute = function (routeId, context) {
+    return (dispatch) => {
         return fetch(Properites.API.ROOT + 'routes/' + routeId + '/entertainments/')
             .then(response => response.json())
             .then(
-                json =>
-                {
+                json => {
                     var items = json._embedded.entertainments;
                     // Рисуем маршрут на карте и вносим его в менюшку
                     dispatch(RouteActions.setRouteListAndRenderPath(items));
@@ -68,8 +60,7 @@ const _loadRoute = function(routeId, context)
 };
 
 const DELETE_HISTORY_ITEM = "DELETE_HISTORY_ITEM";
-const deleteHistoryItem = function (routeId)
-{
+const deleteHistoryItem = function (routeId) {
     return {
         type: DELETE_HISTORY_ITEM,
         payload: {
@@ -78,14 +69,11 @@ const deleteHistoryItem = function (routeId)
     };
 };
 
-const deleteRouteAndUpdateHistory = function(routeId)
-{
-    return (dispatch) =>
-    {
+const deleteRouteAndUpdateHistory = function (routeId) {
+    return (dispatch) => {
         return fetch(Properites.API.ROOT + 'routes/' + routeId, {method: 'DELETE', mode: 'same-origin'})
             .then(
-                response =>
-                {
+                response => {
                     dispatch(deleteHistoryItem(routeId));
                     console.log("Route with id '" + routeId + "' deleted.");
                 }
@@ -94,8 +82,7 @@ const deleteRouteAndUpdateHistory = function(routeId)
 };
 
 const FETCH_ROUTE_SUMMARY_VALIDATE = 'FETCH_ROUTE_SUMMARY_VALIDATE';
-const fetchRouteSummaryValidate = function(routeId, summary)
-{
+const fetchRouteSummaryValidate = function (routeId, summary) {
     return {
         type: FETCH_ROUTE_SUMMARY_VALIDATE,
         payload: {
@@ -105,16 +92,13 @@ const fetchRouteSummaryValidate = function(routeId, summary)
     }
 };
 
-const fetchRouteSummary = function(route)
-{
-    return (dispatch) =>
-    {
+const fetchRouteSummary = function (route) {
+    return (dispatch) => {
         const takePart = (link) => fetch(link).then(response => response.json());
 
         return Promise.all([takePart(route._links.first.href), takePart(route._links.last.href)])
             .then(
-                (values) =>
-                {
+                (values) => {
                     dispatch(fetchRouteSummaryValidate(route.id, {first: values[0], last: values[1]}));
                 }
             );
